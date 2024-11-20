ae4cdff594b75a1d49d6172a307867918a300f7d
cami-negrete <camila.negrete@gmail.com>
2024-11-20 20:43:45 -0300

    nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
-    def configure_permited_parameters
+    def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
         devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
-
-    end
+      end
 end
